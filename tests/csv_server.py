f955dd447a5225fad774392c82dc9755e2a7a1fb
BLuLaptop <brendannlu5@gmail.com>
2023-10-16 20:50:55 +1100

    autoformat

--- tests/csv_server.py
+++ tests/csv_server.py
@@ -8,17 +8,17 @@ BUFFER_SIZE = 1024
 
 class CSVHandler(socketserver.BaseRequestHandler):
     def __init__(self, request, client_address, server):
-        self.byte_buffer = b''
+        self.byte_buffer = b""
         self.df = pd.DataFrame()
         super(CSVHandler, self).__init__(request, client_address, server)
 
     def handle(self) -> None:
-        self.data = b''
-        while True: 
+        self.data = b""
+        while True:
             self.data = self.request.recv(BUFFER_SIZE)
-            if not self.data: 
+            if not self.data:
                 break
-            else: 
+            else:
                 self.byte_buffer += self.data
         self.df = pd.read_csv(BytesIO(self.byte_buffer))
         print(self.df)
